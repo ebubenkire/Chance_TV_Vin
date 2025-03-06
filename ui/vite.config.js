4005d4f587960285dd06388951f83b2981faccd6
ebubenkire <121179031+ebubenkire@users.noreply.github.com>
2025-03-06 11:29:09 +0000

    New-tow

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/your-repo-name/',
   build: {
     outDir: 'dist',
     sourcemap: false,
